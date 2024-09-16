3a6e10db1e12ac27efa36ca54ebb6ee0685cd625
Christian Abegg <christian.abegg@zuehlke.com>
2024-09-16 08:56:35 +0200

    publish starter to maven local

--- graphql-info-spring-boot-starter/build.gradle.kts
+++ graphql-info-spring-boot-starter/build.gradle.kts
@@ -1,23 +1,40 @@
 plugins {
-    id("java")
+    java
+    `maven-publish`
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
 repositories {
     mavenCentral()
 }
+
+
+// to match the applications java version
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
 
 dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-parent:3.3.1")
     compileOnly("org.springframework.boot:spring-boot-starter-graphql:3.3.1")
 
     annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor:3.3.1")
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.example"
+            artifactId = "graphql-info-spring-boot-starter"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
 }
